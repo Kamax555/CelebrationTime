b16b2bdf7a2cf5dfc773a80f525d4ed15b0b7eb9
Rita <t3lixi00@students.oamk.fi>
2017-04-11 18:59:10 +0300

    modal design

--- imports/ui/first_page/restaurants.js
+++ imports/ui/first_page/restaurants.js
@@ -4,6 +4,8 @@ import { Template } from 'meteor/templating';
 
 import{ Restaurants } from '../../api/collections.js';
 
+
+
 import './search.js';
 import './first_page.html';
 import './restaurants/sauraha.html';
